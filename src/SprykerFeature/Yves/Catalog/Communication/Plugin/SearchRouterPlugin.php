2c0e0060fd56e51a9b3ce0d3f25fa21cccb9968a
René Klatt <rene.klatt@project-a.com>
2015-07-14 06:47:48 +0000

    renamed/moved Yves plugins

--- src/SprykerFeature/Yves/Catalog/Communication/Plugin/SearchRouterPlugin.php
+++ src/SprykerFeature/Yves/Catalog/Communication/Plugin/SearchRouterPlugin.php
@@ -4,11 +4,11 @@
  * (c) Spryker Systems GmbH copyright protected
  */
 
-namespace SprykerFeature\Yves\Catalog\Plugin;
+namespace SprykerFeature\Yves\Catalog\Communication\Plugin;
 
 use Silex\Application;
-use SprykerEngine\Yves\Kernel\AbstractPlugin;
-use SprykerFeature\Yves\Catalog\Router\SearchRouter;
+use SprykerEngine\Yves\Kernel\Communication\AbstractPlugin;
+use SprykerFeature\Yves\Catalog\Communication\Router\SearchRouter;
 
 /**
  * Class SearchRouterPlugin
