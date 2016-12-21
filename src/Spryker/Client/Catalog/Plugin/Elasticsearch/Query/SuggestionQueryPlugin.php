1620c31c582c8c9254a0b2f7315161923b638194
Tamas Nyulas <tamas.nyulas@spryker.com>
2016-12-21 15:47:51 +0100

    CORE-733 Search completion refactoring

--- src/Spryker/Client/Catalog/Plugin/Elasticsearch/Query/SuggestionQueryPlugin.php
+++ src/Spryker/Client/Catalog/Plugin/Elasticsearch/Query/SuggestionQueryPlugin.php
@@ -7,46 +7,6 @@
 
 namespace Spryker\Client\Catalog\Plugin\Elasticsearch\Query;
 
-use Elastica\Query;
-use Elastica\Suggest;
-
 class SuggestionQueryPlugin extends CatalogSearchQueryPlugin
 {
-
-    /**
-     * @return \Elastica\Query
-     */
-    protected function createSearchQuery()
-    {
-        $query = parent::createSearchQuery();
-        $query = $this->setSuggestFulltextSearch($query);
-
-        return $query;
-    }
-
-    /**
-     * @param \Elastica\Query $query
-     *
-     * @return \Elastica\Query
-     */
-    protected function setSuggestFulltextSearch(Query $query)
-    {
-        if (!empty($this->searchString)) {
-            $query->setSuggest($this->createSuggest());
-        }
-
-        return $query;
-    }
-
-    /**
-     * @return \Elastica\Suggest
-     */
-    protected function createSuggest()
-    {
-        $suggest = new Suggest();
-        $suggest->setGlobalText($this->searchString);
-
-        return $suggest;
-    }
-
 }
